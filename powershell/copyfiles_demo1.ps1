49c8a543a9dff9b1e6dc0e9f9ef140d67ff0037e
Göran Sander <goran@ptarmiganlabs.com>
2021-07-02 08:14:54 +0200

    change to ps1 file for copying files

---
+++ powershell/copyfiles_demo1.ps1
@@ -0,0 +1,18 @@
+# Parameters
+param ($paramUsername, $paramPassword, $paramFileShareLetter, $paramFileShareUNC)
+
+# Set file share credentials
+$username = "$paramUsername"
+$password = ConvertTo-SecureString "$paramPassword" -AsPlainText -Force
+
+# Create Powershell credentials object
+$cred = New-Object System.Management.Automation.PSCredential -ArgumentList ($username, $password)
+
+# Mount drive
+New-PSDrive -Name "$paramFileShareLetter" -Root "$paramFileShareUNC" -PSProvider "FileSystem" -Credential $cred
+
+# Copy files
+Copy-Item "scripts" -Destination "$paramFileShareLetter:\ci_demo" -Recurse
+
+# Disconnect drive
+Remove-PSDrive -Name $paramFileShareLetter
